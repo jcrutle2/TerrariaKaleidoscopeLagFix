59da91800529f076dea4e56bc262f7886fd7e2b5
jcrutle2 <143005865+jcrutle2@users.noreply.github.com>
2023-09-28 18:18:14 -0400

    Add files via upload

--- Global/GlobalNPCsTest.cs
+++ Global/GlobalNPCsTest.cs
@@ -1,4 +1,5 @@
 using Terraria;
+using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.Utilities;
@@ -9,9 +10,13 @@ namespace kaleidoscopeFix.Global
 	{
         // The Display Name and Tooltip of this item can be edited in the Localization/en-US_Mods.tutorialMod.hjson file.
 
-        public override void SetDefaults(NPC entity)
+        public override void SetDefaults(NPC npc)
         {
-            entity.buffImmune[316] = true;
+            npc.buffImmune[316] = true;
+        }
+        public override void OnSpawn(NPC npc, IEntitySource source)
+        {
+            npc.buffImmune[316] = true;
         }
     }
 }
